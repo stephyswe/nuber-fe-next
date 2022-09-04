70441a63b9dd3831fb71643e2e76eb9496dfe008
Stephanie <stephaniiehallberg@gmail.com>
2022-09-04 07:25:17 +0200

    feat(tests): remove graphql tests to check github action

--- src/pages/client/Dog.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from 'test-utils';
-
-import DogPage from '@/pages/client/dog.page';
-
-it('should render dog', async () => {
-  render(<DogPage name='Buck' />);
-  expect(await screen.findByText('Loading...')).toBeInTheDocument();
-  expect(await screen.findByText('Buck is a poodle')).toBeInTheDocument();
-});
